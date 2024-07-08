7796ba027b8d74a943ad299992b3c35d6e45c42a
Vincytats <vincenttats@gmail.com>
2024-07-08 10:34:21 -0700

    Commit

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,31 +1,22 @@
 #!/usr/bin/env python3
-"""
-Module 1-concurrent_coroutines
-Contains the function wait_n.
-"""
+""" Takes 2 int args, waits for random delay """
 
 import asyncio
+import random
 from typing import List
-from 0-basic_async_syntax import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Spawns wait_random n times with the specified max_delay.
 
-    Args:
-        n (int): The number of times to spawn wait_random.
-        max_delay (int): The maximum delay in seconds.
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """ Waits for ran delay until max_delay, returns list of actual delays """
+    spawn_list = []
+    delay_list = []
+    for i in range(n):
+        delayed_task = asyncio.create_task(wait_random(max_delay))
+        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
+        spawn_list.append(delayed_task)
 
-    Returns:
-        List[float]: The list of all the delays in ascending order.
-    """
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    
-    sorted_delays = []
-    while delays:
-        min_delay = min(delays)
-        sorted_delays.append(min_delay)
-        delays.remove(min_delay)
-    
-    return sorted_delays
+    for spawn in spawn_list:
+        await spawn
+
+    return delay_list
